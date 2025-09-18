e52067ee5fe02bedf63fa2b01fd303a592d1460a
ivi-dev <videviliyan@gmail.com>
2025-09-18 19:33:26 +0300

    Check fo existing DB users before creating

--- DB/scripts/1-create-users.js
+++ DB/scripts/1-create-users.js
@@ -3,30 +3,45 @@ const fs = require('fs');
 const rootUser = getUserData("root"),
       backendUser = getUserData("backend"),
       dataHarvesterUser = getUserData("data-harvester"),
+      adminDB = "admin",
       flagsDB = "flags",
       cluster = Mongo();
 
 db = cluster.getDB("admin");
 
-db.auth(rootUser.user, rootUser.pwd);
+// db.auth(rootUser.user, rootUser.pwd);
      
+// Create the "ROOT" user
+tryCreateUser(db, rootUser, [ { role: "root", db: adminDB } ]);
+
+// db.createUser({
+//     ...rootUser,
+//     roles: [
+//         { role: "root", db: adminDB }
+//     ],
+// });
+
 db = db.getSiblingDB(flagsDB);
 
 // Create the "DATA-HARVESTER" user
-db.createUser({
-    ...dataHarvesterUser,
-    roles: [
-        { role: "readWrite", db: flagsDB }
-    ],
-});
+tryCreateUser(db, dataHarvesterUser, [ { role: "readWrite", db: flagsDB } ]);
+
+// db.createUser({
+//     ...dataHarvesterUser,
+//     roles: [
+//         { role: "readWrite", db: flagsDB }
+//     ],
+// });
 
 // Create the "BACKEND" user
-db.createUser({
-    ...backendUser,
-    roles: [
-        { role: "read", db: flagsDB }
-    ],
-});
+tryCreateUser(db, backendUser, [ { role: "read", db: flagsDB } ]);
+
+// db.createUser({
+//     ...backendUser,
+//     roles: [
+//         { role: "read", db: flagsDB }
+//     ],
+// });
 
 function getUserData(type) {
     const userTypes = ["root", "backend", "data-harvester"];
@@ -41,4 +56,20 @@ function getUserData(type) {
         user: username.trim(),
         pwd: password.trim(),
     };
+}
+
+function userExists(db, username) {
+    const users = db.getUsers();
+    return users.some(user => user.user === username);
+}
+
+function tryCreateUser(db, userData, roles) {
+    if (userExists(db, userData.user)) {
+        print(`User ${userData.user} already exists. Skipping creation.`);
+        return;
+    }
+    db.createUser({
+        ...userData,
+        roles: roles
+    });
 }
