741f0c35cc7809b10b932411eff7201a7787d167
ivi-dev <videviliyan@gmail.com>
2025-09-18 19:33:02 +0300

    Add DB init script stub

---
+++ DB/scripts/init-db.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# ===============================================================
+# Initialize the MongoDB database, creating the necessary users 
+# and roles if they don't already exist.
+# ===============================================================
+
+# TODO: ...
+# mongosh --ssl \
+# 	    --sslCAFile /etc/ssl/certs/flags/root.crt \
+#         .../1-create-users.js
