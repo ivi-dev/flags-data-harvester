3abab38d1d39aa85024564fb910ed4061b4ceeca
ivi-dev <videviliyan@gmail.com>
2025-09-20 10:18:54 +0300

    Don't assign group ownership to files

--- DB/scripts/setup-db.sh
+++ DB/scripts/setup-db.sh
@@ -9,7 +9,7 @@
 if [ ! -f "$LOG_PATH" ]; then
     echo "Creating the MongoDB log file at $LOG_PATH"
     touch $LOG_PATH
-    chown $SYSTEM_DB_USER:$SYSTEM_DB_GROUP $LOG_PATH
+    chown $SYSTEM_DB_USER $LOG_PATH
     chmod 700 $LOG_PATH
 fi
 
@@ -18,7 +18,7 @@ if [ ! -d "$DATA_PATH" ]; then
     echo "Creating the MongoDB data directory at $DATA_PATH"
     mkdir -p $DATA_PATH
 fi
-chown -R $SYSTEM_DB_USER:$SYSTEM_DB_GROUP $DATA_PATH
+chown -R $SYSTEM_DB_USER $DATA_PATH
 chmod -R 700 $DATA_PATH
 
 # 3. Create the mongod pid file if it doesn't exist
@@ -26,9 +26,9 @@ if [ ! -f "$PID_PATH" ]; then
     echo "Creating the MongoDB pid file at $PID_PATH"
     parent_dir=$(dirname "$PID_PATH")
     mkdir -p $parent_dir
-    chown $SYSTEM_DB_USER:$SYSTEM_DB_GROUP $parent_dir
+    chown $SYSTEM_DB_USER $parent_dir
     chmod 700 $parent_dir
     touch $PID_PATH
-    chown $SYSTEM_DB_USER:$SYSTEM_DB_GROUP $PID_PATH
+    chown $SYSTEM_DB_USER $PID_PATH
     chmod 700 $PID_PATH
 fi
